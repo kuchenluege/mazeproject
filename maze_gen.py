cffe018e4e886a998b003647919b3a8037579bfb
kuchenluege <tobias.knueven@gmail.com>
2021-12-02 10:06:32 -0500

    30x30

--- maze_gen.py
+++ maze_gen.py
@@ -3,7 +3,7 @@ import random
 from mazelib.generate import BacktrackingGenerator as bg
 import pygame as pg
 
-maze_gen = bg.BacktrackingGenerator(100, 100)
+maze_gen = bg.BacktrackingGenerator(30, 30)
 grid = maze_gen.generate()
 start = (0, 1)
 finish = (grid.shape[0] - 1, grid.shape[1] - 2)
